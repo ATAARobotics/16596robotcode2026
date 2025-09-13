441cf699cf0a60294fa78c0df04f50e7e772abc3
George Nazarey <gnazarey@naz.com>
2025-09-13 12:14:57 -0600

    Making directory structure and added driver files

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanisms/Constants.java
@@ -0,0 +1,99 @@
+package org.firstinspires.ftc.teamcode.Mechanisms;
+
+public class
+
+
+Constants {
+    public static final boolean DASHBOARD_ENABLED = true;       // this needs to be false for competitions!!
+    public static final boolean TELEMETRY_ENABLED = true;
+    public static final boolean JUST_TESTING=true;      // will be used to insert temporary test code
+
+    public static final double TICKS_TO_INCHES = Math.PI * 48 / (25.4 * 2000);                      // for use in Odometry
+    public static final double SPEED_RATIO = 1.0;  // Use this to slow down robot
+    public static final double TURN_RATIO = 1.0; // use this to slow turn rate
+
+
+    public static final double AUTO_DRIVE_SPEED = 0.3;
+    public static final double AUTO_STEP_DELAY = 2.0;
+
+
+
+    //
+    // ========== Define Drive constants.  Make them public so they CAN be used by the calling OpMode
+    public static final double FORWARD = 0; // north
+    public static final double BACK = 180; // south
+    public static final double RIGHT = -90; // east
+    public static final double LEFT = 90; //west
+    public static final double NORTH = 0;
+    public static final double SOUTH = 180;
+    public static final double EAST = -90;
+    public static final double WEST = 90;
+    public static final double NORTH_EAST = -45; // North East
+    public static final double SOUTH_EAST = -135; // South East
+    public static final double SOUTH_WEST = 135; // South West
+    public static final double NORTH_WEST = 45; // North West
+
+    //   SERVO CONSTANTS  // finger servo closed position
+    // public static final double STEP = 0.05; // used for manual increment of wrist servo
+    // public static final double  LF_CLOSED       =  0.3 ;// finger servo closed position
+    public static final double  LF_OPEN    =  0.7 ; // open position
+    public static final double  RF_CLOSED       =  0.6 ;
+    public static final double  RF_OPEN    =  0.2 ; // open position*/
+// for elevator moves
+    public static final int ELEVATOR_START  =  0;
+    public static final int ELEVATOR_PICKUP_SPECIMEN = 40;
+    public static final int ELEVATOR_HIGH_BAR = 1833;  // nov 22
+
+    // public static final int ELEVATOR_LOW_BAR= 350;  // nov 22
+
+    public static final int ELEVATOR_HIGH_BASKET = 3578;
+    public static final int ELEVATOR_MAX = 4337;
+    public static final int ELEVATOR_MIN = 0;
+    public static final double ELEVATOR_SPEED_FACTOR = 0.7; // 1.0 Old elevator and 0.3 for new elevator
+//    public static final double ELEVATOR_POSITION_TOLERANCE = 15.0;
+//    public static final double ELEVATOR_POSITION_COEFFICIENT = 0.05;
+//    public static final int ELEVATOR_STUCK_COUNT = 5;
+    // Claw constants
+    //robot 2 need to be fixed, values were revered
+    public static boolean CLAW_OPEN = true;
+    public static boolean CLAW_CLOSED = false;
+    public static final double SERVO_CLAW_OPEN = 0.2 ;
+    public static final double SERVO_CLAW_CLOSED = 0.8 ;
+
+// ===========  for elbow movements ======================
+//robot 2 need to be fixed, values were revered
+   // public static final double ELBOW_BOTTOM = 0.3;
+    public static final double ELBOW_START_POSITION = 0.0;
+//    public static final double ELBOW_PICKUP_SAMPLE = 1.0;
+    public static final double ELBOW_PICKUP_SPECIMEN = 0.4;
+    //Pickup specimen from the wall for CompBot (Tortuga)
+    public static final double ELBOW_PICKUP_SPECIMEN_WALL = 0.5;
+    public static final double ELBOW_DEPOSIT_SAMPLE_HIGH_BUCKET = 0.67;
+
+    public static int ELBOW_MAX = 3;
+    public static int ELBOW_MIN = 0;
+    //
+// Tolerance Section
+    public static final double JOYSTICK_TOLERANCE = 0.5;
+    public static final double AT_XY_TOLERANCE = 25.0;
+    public static double AUTO_Y_DISTANCE_ERROR = 15.0;
+    public static double AUTO_X_DISTANCE_ERROR = 15.0;
+    public static final double DRIVE_PID_ERROR = 1.5; // inches?? Only used in PractiseDriveTrain??
+    public static double HEADING_ERROR_Tolerance = 5.0; // Degrees... this now matches PID tolerance
+    // Bruce:sept30: change these for use in elevator?kk
+//    public static final double ARM_RADIANS_PER_TICK = Math.PI/244.0;
+//    public static final double ARM_TICKS_PER_90DEG = 113;   // CONFIRM #TICKS PER 90.
+   // public static final double DRIVE_PID_TOLERANCE = 1;//testing changed from .5 to 1
+    //odometer constants
+    public static int ODOMETER_PRACTICE_MODE = 1;
+    public static int ODOMETER_COMPETITION_MODE = 8;
+    public static double ODOMETER_X_OFFSET = 0;// left offset is +  uppdated after contact with gobilda support
+    public static double ODOMETER_Y_OFFSET = 24.0;
+    // auto constants
+    public static double XPID_Kp = 0.018;// was 0.14
+    public static double YPID_Kp = 0.018;
+    public static double HEADING_Kp = 0.015; // was 0.01
+
+
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanisms/DriveTrain2025.java
@@ -0,0 +1,224 @@
+package org.firstinspires.ftc.teamcode.Mechanisms;
+
+
+import com.acmerobotics.dashboard.config.Config;
+import com.arcrobotics.ftclib.controller.PIDController;
+import com.arcrobotics.ftclib.drivebase.MecanumDrive;
+import com.arcrobotics.ftclib.hardware.SimpleServo;
+import com.arcrobotics.ftclib.hardware.motors.Motor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.PIDCoefficients;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
+
+@Config
+public class DriveTrain2025 {
+
+    // Driving Motors
+    private final Motor leftFrontDrive;
+    private final Motor rightFrontDrive;
+    private final Motor leftBackDrive;
+    private final Motor rightBackDrive;
+
+
+    //servos
+    private final SimpleServo claw;
+    private final SimpleServo elbow;
+    MecanumDrive driveBase;
+
+    public GoBildaPinpointDriver odometer;
+    public PIDController headingControl = null;
+    public static PIDCoefficients headingpid = new PIDCoefficients(Constants.HEADING_Kp, 0.001, 0.000);
+    public static PIDCoefficients xpid = new PIDCoefficients(Constants.XPID_Kp, 0.0, 0.00);
+    public static PIDCoefficients ypid = new PIDCoefficients(Constants.YPID_Kp, 0.0, 0.00);
+    public PIDController xControl = null;
+    public PIDController yControl = null;
+
+    public double headingCorrection = 0;
+
+    public double headingSetPoint = Constants.FORWARD;
+    public double heading;
+
+    private boolean autoEnabled = false;
+
+    HardwareMap hwMap;
+    private double currentSpeed = 0;
+    private double currentXTarget = 0;
+    private double currentYTarget = 0;
+    double xSpeed = 0;
+    double ySpeed = 0;
+    public double headingError;
+
+    public DriveTrain2025(HardwareMap hwMap) {
+        this.hwMap = hwMap;
+
+        // Define and Initialize Motors (note: need to use reference to actual OpMode).
+
+        leftFrontDrive = new Motor(hwMap, "left_front_drive"); // 0
+        rightFrontDrive = new Motor(hwMap, "right_front_drive"); // 1
+        leftBackDrive = new Motor(hwMap, "left_back_drive"); // 2
+        rightBackDrive = new Motor(hwMap, "right_back_drive"); // 3
+        //elevator = new Motor(hwMap,"elevator");
+        claw = new SimpleServo(hwMap, "claw", 0, 1); // expansion hub port 0
+        elbow = new SimpleServo(hwMap, "elbow", 0, 1); // expansion hub port 1
+
+        //elevator.resetEncoder();
+
+
+        // xPod = new Motor(hwMap, "x-pod"); // 3
+        //  yPod = new Motor(hwMap, "y-pod"); // 2
+
+
+        driveBase = new MecanumDrive(leftFrontDrive, rightFrontDrive, leftBackDrive, rightBackDrive);
+
+
+    }
+
+    public void init() {
+        headingControl = new PIDController(headingpid.p, headingpid.i, headingpid.d);
+        headingControl.setTolerance(Constants.HEADING_ERROR_Tolerance);// was 3 increased to see if affects spinnning ..cbw
+        xControl = new PIDController(xpid.p, xpid.i, xpid.d);//FOR AUTO
+        yControl = new PIDController(ypid.p, ypid.i, ypid.d);//For AUTO
+
+        // redundant as default is brake mode
+        leftBackDrive.setZeroPowerBehavior(Motor.ZeroPowerBehavior.BRAKE);
+        rightBackDrive.setZeroPowerBehavior(Motor.ZeroPowerBehavior.BRAKE);
+        rightFrontDrive.setZeroPowerBehavior(Motor.ZeroPowerBehavior.BRAKE);
+        leftFrontDrive.setZeroPowerBehavior(Motor.ZeroPowerBehavior.BRAKE);
+        // Inverting because of the extra gear and not direct drive
+        rightFrontDrive.setInverted(true);
+        rightBackDrive.setInverted(true);
+        leftFrontDrive.setInverted(true);
+        leftBackDrive.setInverted(true);
+        // odometer initializing -- this should go into PracticeDriveTrain2025??
+        odometer = hwMap.get(GoBildaPinpointDriver.class, "xy-cord");
+        odometer.setOffsets(Constants.ODOMETER_X_OFFSET, Constants.ODOMETER_Y_OFFSET);
+        odometer.setEncoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_SWINGARM_POD);
+        odometer.setEncoderDirections(GoBildaPinpointDriver.EncoderDirection.FORWARD, GoBildaPinpointDriver.EncoderDirection.FORWARD);
+
+
+    }// end of init()
+
+    public void start() {
+
+    }
+
+    public void loop() {
+        //heading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.DEGREES);
+        odometer.update();      // needs to be called once per loop
+        Pose2D pos = odometer.getPosition();
+        heading = pos.getHeading(AngleUnit.DEGREES);
+        // Because it's a double, can't check for exactly 180, so we check if it's almost 180 in either direction.
+        if (Math.abs(Math.abs(headingSetPoint) - 180.0) < Constants.HEADING_ERROR_Tolerance) {
+
+            // "south" is special because it's around the 180/-180 toggle-point
+            // Change set-point between 180/-180 depending on which is closer.
+            if (heading < 0.0) {
+                headingSetPoint = -180;
+            } else {
+                headingSetPoint = 180;
+            }
+        }
+
+        // PID controller for heading
+        headingControl.setSetPoint(headingSetPoint);
+        headingCorrection = -headingControl.calculate(heading);// confirm if (-) is needed.
+        headingError = Math.abs(headingSetPoint - heading);
+//temporary test code
+        if (headingError <= Constants.HEADING_ERROR_Tolerance) {
+            headingCorrection = 0;
+        }
+
+        // need to send 0 correction if 'Happy"
+
+
+        driveBase.driveFieldCentric(
+                xSpeed,// strafe
+                ySpeed,//forward
+                headingCorrection,// turn
+                heading,// heading
+                false);
+    }// end of loop()
+
+//    public boolean atTarget() { // Pythagorean theorem to get the distance from target as a number.
+//        double xError = getXPosition() - currentXTarget;
+//        double yError = getYPosition() - currentYTarget;
+//        return !autoEnabled || Math.sqrt(xError * xError + yError * yError) < Constants.DRIVE_PID_ERROR;
+//    }
+
+    //============== Move in new Direction ==========
+    public void setDirection(double newHeading) {
+        headingSetPoint = newHeading;
+    }
+    public void setFacing(double newHeading) { headingSetPoint = newHeading; }
+    public boolean onHeading() {
+        return Math.abs(heading - headingSetPoint) < Constants.HEADING_ERROR_Tolerance;
+
+    }
+    public void driveTo(double speed, double xDist, double yDist, double facing) {
+        autoEnabled = true;
+        currentSpeed = speed;
+        currentXTarget = xDist;
+        currentYTarget = yDist;
+        setDirection(facing);
+
+        xControl.setSetPoint(currentXTarget);
+        yControl.setSetPoint(currentYTarget);
+
+    }
+
+    public void drive(double forwardSpeed,  double strafeSpeed) {
+        // tell ftclib its inputs  strafeSpeed,forwardSpeed,turn,heading
+        // turn and heading are managed in loop with the heading control PID
+        // inverting strafe speed to correct directions, inverting no longer required for telemtry pdos have been turned to right position
+        xSpeed = -strafeSpeed;
+        ySpeed = forwardSpeed;
+    }
+
+    public double getXPosition() { // Convert xPod into actual direction based on current heading
+        double xPos = 0.0;
+        Pose2D pos = odometer.getPosition();
+        xPos = pos.getX(DistanceUnit.MM);
+        return -xPos;// pod mounted backwards
+    }
+    public double getYPosition() { // Convert yPod into actual direction based on current heading
+        double yPos = 0.0;
+        Pose2D pos = odometer.getPosition();
+        // The following is needed because of an error in the driver
+        yPos = pos.getY(DistanceUnit.MM);
+
+        return -yPos;// pod mounted backwards
+    }
+
+    public void stop() {
+        xSpeed = 0.0;
+        ySpeed = 0.0;
+        driveBase.stop();
+        autoEnabled = false;
+    }
+
+    public void printTelemetry(Telemetry telemetry) {
+        //telemetry.addData("actual heading:", "%5.2f", imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.DEGREES));
+        telemetry.addData("saved heading:", "%5.2f", heading);
+        telemetry.addData("heading Target:", headingSetPoint);
+        telemetry.addData("heading correction:", headingCorrection);
+        telemetry.addData("X Distance inches:", "%5.2f", getXPosition());
+        telemetry.addData("Y Distance inches:", "%5.2f", getYPosition());
+        telemetry.addData("Auto DriveTo Enabled", autoEnabled);
+
+       // telemetry.addData("elevator","%5.2f",elevator.getCurrentPosition());
+
+        if(autoEnabled) {
+            telemetry.addData("Auto target X:", "%5.2f", currentXTarget);
+            telemetry.addData("Auto target Y:", "%5.2f", currentYTarget);
+            telemetry.addData("heading OnTarget:", onHeading());
+          //  telemetry.addData("lateral OnTarget:", atTarget());
+            telemetry.update();
+        }
+    }
+
+
+}
