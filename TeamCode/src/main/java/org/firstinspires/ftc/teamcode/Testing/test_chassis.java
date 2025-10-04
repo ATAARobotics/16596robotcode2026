b44864263761784fb2e4263df2ac4d661f2e3e08
ftc16596 <ftc16596@ataarobotics.ca>
2025-10-04 10:20:11 -0600

    added graphing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/test_chassis.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/test_chassis.java
@@ -84,6 +84,8 @@ test_chassis extends OpMode {
     private GraphManager graphManager;
     public double speed = 0.0;
 
+    public double speed2 = 0.0;
+
     /* private double heading; */
 
     @Override
@@ -119,9 +121,11 @@ test_chassis extends OpMode {
         operator.readButtons();
         driveTrain.loop(); // Current elbow
         speed = driveTrain.shooter.getCorrectedVelocity();// what is corrected velocity??
+        speed2 = driveTrain.shooter2.getCorrectedVelocity();
         joinedTelemetry.addData("Shooter Speed", speed); //telemetry shooter speed
         graphManager.addData("Shooter Speed",speed);
-
+        joinedTelemetry.addData("Shooter Speed", speed2); //telemetry shooter2 speed
+        graphManager.addData("Shooter Speed",speed2);
         //======= get human inputs for drive=============
 
         double strafeSpeed = -driver.getLeftX() * Constants.SPEED_RATIO;
