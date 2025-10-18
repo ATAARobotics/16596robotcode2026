1a4e2f64ea3870c010c4cf1debbebbccfb7af1c9
George Nazarey <gnazarey@naz.com>
2025-10-17 20:42:42 -0600

    Added a constant for the April Tag testing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanisms/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanisms/Constants.java
@@ -106,6 +106,7 @@ Constants {
     // Limelight Settings
     public static int LIMELIGHT_APRIL_TAG_BLUE = 0;
     public static int LIMELIGHT_APRIL_TAG_RED = 1;
+    public static int LIMELIGHT_APRIL_TESTING = 2;
     public static double LIMELIGHT_GOAL_HEIGHT_INCHES = 29.50;
     public static double LIMELIGHT_LENS_HEIGHT_INCHES = 8.25;
     public static double LIMELIGHT_MOUNT_ANGLE_DEGREE = 1.0;
