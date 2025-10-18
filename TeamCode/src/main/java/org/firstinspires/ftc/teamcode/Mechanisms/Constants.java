2bdf9acfbde255f1c9230d5b9c98057261747ac3
George Nazarey <gnazarey@naz.com>
2025-10-17 19:21:36 -0600

    Added Color library

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanisms/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanisms/Constants.java
@@ -106,4 +106,20 @@ Constants {
     public static double LIMELIGHT_LENS_HEIGHT_INCHES = 8.25;
     public static double LIMELIGHT_MOUNT_ANGLE_DEGREE = 1.0;
 
+    public static final class RGB_Light {
+        public static final double OFF = 0.0;
+        public static final double BLACK = 0.0;
+        public static final double RED = 0.279;
+        public static final double ORANGE = 0.333;
+        public static final double YELLOW = 0.388;
+        public static final double SAGE = 0.444;
+        public static final double GREEN = 0.5;
+        public static final double AZURE = 0.555;
+        public static final double BLUE =  0.611;
+        public static final double INDIGO = 0.666;
+        public static final double VIOLET = 0.722;
+        public static final double WHITE = 1.000;
+        public static final double ON = 1.000;
+
+    }
 }
