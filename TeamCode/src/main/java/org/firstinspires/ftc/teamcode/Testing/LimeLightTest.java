889d0e0ccc3a53b6e52a155fa005024990dc9094
George Nazarey <gnazarey@naz.com>
2025-10-17 21:13:17 -0600

    Added indicator light when AprilTag is detected

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/LimeLightTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/LimeLightTest.java
@@ -7,6 +7,7 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.Mechanisms.Constants;
+import org.firstinspires.ftc.teamcode.Subsystem.LightIndicator;
 
 // The following libraries are special for LimeLight
 import com.qualcomm.hardware.limelightvision.LLResult;
@@ -22,6 +23,8 @@ public class LimeLightTest extends OpMode {
 //test comment
     private Limelight3A limelight;
     private JoinedTelemetry joinedTelemetry;
+    // Testing Indicator
+    private LightIndicator indicator;
 
     @Override
     public void init() {
@@ -37,12 +40,14 @@ public class LimeLightTest extends OpMode {
         joinedTelemetry = new JoinedTelemetry(telemetry,panelsTelemetry.getTelemetry().getWrapper(),dashboard.getTelemetry());
         joinedTelemetry.addLine("LimeLight Initialized");
         joinedTelemetry.update();
+        // Indicator light
+        indicator = new LightIndicator(hardwareMap);
     }
     @Override
     public void start() {
 
         limelight.start(); // This tells Limelight to start looking!
-        limelight.pipelineSwitch(Constants.LIMELIGHT_APRIL_TAG_BLUE);
+        limelight.pipelineSwitch(Constants.LIMELIGHT_APRIL_TESTING);
 
     }
     @Override
@@ -52,12 +57,15 @@ public class LimeLightTest extends OpMode {
 
         // Send data to telemetry
         if (limeLightResults != null && limeLightResults.isValid()) {
+            indicator.setColor(Constants.RGB_Light.ON);
+            joinedTelemetry.addData("Indicator Light", indicator.getColor());
             joinedTelemetry.addData("Target X", limeLightResults.getTx()); // How far left or right the target is (degrees)
             joinedTelemetry.addData("Target Y", limeLightResults.getTy()); // How far up or down the target is (degrees)
             joinedTelemetry.addData("Target Area", limeLightResults.getTa()); // How big the target looks (0%-100% of the image)
             joinedTelemetry.addData("Limelight Pipeline Index", limeLightResults.getPipelineIndex());
             joinedTelemetry.addData("Distance to Target inches", target_distance(limeLightResults));
         } else {
+            indicator.setColor(Constants.RGB_Light.OFF);
             joinedTelemetry.addData("Limelight", "No Targets");
             joinedTelemetry.addData("Limelight Pipeline Index", limeLightResults.getPipelineIndex());
         }
