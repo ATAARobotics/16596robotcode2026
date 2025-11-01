eaa2ceb2a29e3b2f030b28844350368bdd425198
ftc16596 <ftc16596@ataarobotics.ca>
2025-11-01 13:46:01 -0600

    testing for george

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanisms/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanisms/Constants.java
@@ -20,7 +20,7 @@ public class Constants {
     // ========== Intake, Shooter and Feeder Speeds
     //
     public static final double INTAKE_SPEED = 1.0;
-    public static final double FLYWHEEL_SPEED = 0.7;// this needs to be verified; only far was test on Oct 25 -CBW
+    public static final double FLYWHEEL_SPEED = 0.6;// in RPM --this needs to be verified; only far was test on Oct 25 -CBW
     public static final double FLYWHEEL_CLOSE = 0.7;
     public static final double FLYWHEEL_FAR = 0.71;
     public static final double FLYWHEEL_RECOVERY = 1750.0;
