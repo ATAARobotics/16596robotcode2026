9fde856ac80c3a924896e76cf39bd9299570d427
ftc16596 <ftc16596@ataarobotics.ca>
2026-02-03 19:32:47 -0700

    Made sure that both are the same except for xy coord.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/BlueFarShootingAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/BlueFarShootingAuto.java
@@ -1,14 +1,11 @@
 package org.firstinspires.ftc.teamcode.Auto;
 
-import static org.firstinspires.ftc.teamcode.Mechanisms.Constants.BLUE_FAR_SHOOTING_MOVE_OFF_WALL_X;
-
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.arcrobotics.ftclib.gamepad.GamepadKeys;
 import com.bylazar.telemetry.JoinedTelemetry;
 import com.bylazar.telemetry.PanelsTelemetry;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
 import org.firstinspires.ftc.teamcode.Mechanisms.Constants;
@@ -19,7 +16,6 @@ import org.firstinspires.ftc.teamcode.Subsystem.LightIndicatorSubsystem;
 public class BlueFarShootingAuto extends OpMode {
     private DriveTrainBasic driveTrain;
     public LightIndicatorSubsystem indicator;
-
     private JoinedTelemetry joinedTelemetry;
     private double startTime;
     private Location currentDestination;
@@ -53,14 +49,13 @@ public class BlueFarShootingAuto extends OpMode {
         driveTrain.loop();
         Pose2D pos = driveTrain.odometer.getPosition();
         if (!autoDone) {
-            driveTrain.intake.set(Constants.INTAKE_SPEED);
+            driveTrain.intake.set(Constants.INTAKE_SPEED_AUTO);
             switch (currentWayPoint) {
-
                 case Move_Off_Wall:
                     driveTrain.feed1.setPower(-Constants.FEED_SPEED);
                     driveTrain.feed2.setPower(-Constants.FEED_SPEED);
                     // Set Destination
-                    currentDestination.x = BLUE_FAR_SHOOTING_MOVE_OFF_WALL_X;//move forward towards the tape
+                    currentDestination.x = Constants.BLUE_FAR_SHOOTING_MOVE_OFF_WALL_X;//move forward towards the tape
                     currentDestination.x_speed = 0.5;
                     currentDestination.y = -121.0; //current value needs to be test, was -121
                     currentDestination.y_speed = 0.5;
@@ -91,30 +86,26 @@ public class BlueFarShootingAuto extends OpMode {
                 case Far_Shot://Shoots the artifacts
                     // Currently not running as of Nov 22 2025
                     driveTrain.shooter.set(Constants.FLYWHEEL_FAR_AUTO);
-                    if (driveTrain.canLaunch(Constants.FLYWHEEL_FAR_AUTO)) {
+                    if (driveTrain.canLaunch(Constants.FLYWHEEL_FAR_AUTO))
+                    {
                         indicator.setColor(Constants.RGB_Light.GREEN);
                         driveTrain.feed1.setPower(Constants.FEED_SPEED);
                         driveTrain.feed2.setPower(Constants.FEED_SPEED);
-                        // creates an if statement based on the flywheel recovery speed so it only shoots when it's up to speed
-                        if(Constants.FLYWHEEL_FAR_AUTO >= Constants.FLYWHEEL_RECOVERY){
-                            driveTrain.feed2.setPower(Constants.FEED_SPEED);
-                        }
-                    } else {
-                        indicator.setColor(Constants.RGB_Light.RED);
                     }
-
-                    if (getRuntime() - startTime >= 16.7) {
+                    else
+                    {
+                        indicator.setColor(Constants.RGB_Light.RED);
+                        driveTrain.feed1.setPower(0.0);
+                        driveTrain.feed2.setPower(0.0);
+                    }
+                    if (getRuntime() - startTime >= 16.7)
+                    {
                         driveTrain.shooter.set(0.0); //this doesnt stop at the end either-we need it to stop
-                        driveTrain.shooter.stopMotor();
+                        driveTrain.stopFlyWheel();
                         driveTrain.feed1.setPower(0.0);
                         driveTrain.feed2.setPower(0.0);
                         driveTrain.intake.set(0); //this doesn't stop at the end- we need it to stop
                         currentWayPoint = WayPoints.Move_Off_White_Tape;
-                    }
-                        else{ //rechecks constantly if under that amount of time
-                        if(Constants.FLYWHEEL_FAR_AUTO >= Constants.FLYWHEEL_RECOVERY) {
-                            driveTrain.feed2.setPower(Constants.FEED_SPEED);
-                        }
                     }
                     break;
                 case Move_Off_White_Tape:
@@ -146,6 +137,8 @@ public class BlueFarShootingAuto extends OpMode {
                         this.goto_xy(currentDestination);
                         //stops everything
                       driveTrain.shooter.set(0.0);
+         //               driveTrain.feed1.setPower(0.0);
+          //              driveTrain.feed2.setPower(0.0);
                       driveTrain.intake.set(0);
                     } else {
                         driveTrain.drive(0.0, 0.0);
@@ -155,7 +148,6 @@ public class BlueFarShootingAuto extends OpMode {
                 case Done:
                 default:
                     driveTrain.setFacing(Constants.NORTH);
-                    //driveTrain.shooter.stopMotor();
                     driveTrain.stop();
                     autoDone = true;
                     break;
@@ -201,7 +193,6 @@ public class BlueFarShootingAuto extends OpMode {
         // New Way to get there
         driveTrain.xControl.setSetPoint(targetLocation.x);
         driveTrain.yControl.setSetPoint(targetLocation.y);
-
         joinedTelemetry.addData("X Speed",driveTrain.xControl.calculate(driveTrain.getXPosition()) * Constants.AUTO_DRIVE_SPEED);
         joinedTelemetry.addData("Y Speed",driveTrain.yControl.calculate(driveTrain.getYPosition()) * Constants.AUTO_DRIVE_SPEED);
         driveTrain.drive(driveTrain.xControl.calculate(driveTrain.getXPosition()) * Constants.AUTO_DRIVE_SPEED, driveTrain.yControl.calculate(driveTrain.getYPosition()) * Constants.AUTO_DRIVE_SPEED);
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/RedFarShootingAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/RedFarShootingAuto.java
@@ -1,11 +1,11 @@
 package org.firstinspires.ftc.teamcode.Auto;
 
 import com.acmerobotics.dashboard.FtcDashboard;
+import com.arcrobotics.ftclib.gamepad.GamepadKeys;
 import com.bylazar.telemetry.JoinedTelemetry;
 import com.bylazar.telemetry.PanelsTelemetry;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
 import org.firstinspires.ftc.teamcode.Mechanisms.Constants;
@@ -16,12 +16,12 @@ import org.firstinspires.ftc.teamcode.Subsystem.LightIndicatorSubsystem;
 public class RedFarShootingAuto extends OpMode {
     private DriveTrainBasic driveTrain;
     public LightIndicatorSubsystem indicator;
-
     private JoinedTelemetry joinedTelemetry;
     private double startTime;
     private Location currentDestination;
     private boolean autoDone;
     private WayPoints currentWayPoint = WayPoints.Move_Off_Wall;
+    public double speed = 0.0;
 
     @Override
     public void init() {
@@ -49,10 +49,11 @@ public class RedFarShootingAuto extends OpMode {
         driveTrain.loop();
         Pose2D pos = driveTrain.odometer.getPosition();
         if (!autoDone) {
-            driveTrain.intake.set(Constants.INTAKE_SPEED);
+            driveTrain.intake.set(Constants.INTAKE_SPEED_AUTO);
             switch (currentWayPoint) {
-
                 case Move_Off_Wall:
+                    driveTrain.feed1.setPower(-Constants.FEED_SPEED);
+                    driveTrain.feed2.setPower(-Constants.FEED_SPEED);
                     // Set Destination
                     currentDestination.x = -250.0; // was 43.0
                     currentDestination.x_speed = 0.5;
@@ -85,33 +86,26 @@ public class RedFarShootingAuto extends OpMode {
                 case Far_Shot://Shoots the artifacts
                     // Currently not running as of Nov 22 2025
                     driveTrain.shooter.set(Constants.FLYWHEEL_FAR_AUTO);
-                    if (driveTrain.canLaunch(Constants.FLYWHEEL_FAR_AUTO)) {
+                    if (driveTrain.canLaunch(Constants.FLYWHEEL_FAR_AUTO))
+                    {
                         indicator.setColor(Constants.RGB_Light.GREEN);
                         driveTrain.feed1.setPower(Constants.FEED_SPEED);
                         driveTrain.feed2.setPower(Constants.FEED_SPEED);
-                        // creates an if statement based on the flywheel recovery speed so it only shoots when it's up to speed
-                        if(Constants.FLYWHEEL_FAR_AUTO >= Constants.FLYWHEEL_RECOVERY){
-                            driveTrain.feed2.setPower(Constants.FEED_SPEED);
-                        }
-//                    else {
-//                        driveTrain.feed2.setPower(0);
-//                    }
-                    } else {
-                        indicator.setColor(Constants.RGB_Light.RED);
                     }
-
-                    if (getRuntime() - startTime >= 16.7) {
+                    else
+                    {
+                        indicator.setColor(Constants.RGB_Light.RED);
+                        driveTrain.feed1.setPower(0.0);
+                        driveTrain.feed2.setPower(0.0);
+                    }
+                    if (getRuntime() - startTime >= 16.7)
+                    {
                         driveTrain.shooter.set(0.0); //this doesnt stop at the end either-we need it to stop
+                        driveTrain.stopFlyWheel();
                         driveTrain.feed1.setPower(0.0);
                         driveTrain.feed2.setPower(0.0);
                         driveTrain.intake.set(0); //this doesn't stop at the end- we need it to stop
-                        driveTrain.stopFlyWheel();
                         currentWayPoint = WayPoints.Move_Off_White_Tape;
-                    }
-                        else{ //rechecks constantly if under that amount of time
-                        if(Constants.FLYWHEEL_FAR_AUTO >= Constants.FLYWHEEL_RECOVERY) {
-                            driveTrain.feed2.setPower(Constants.FEED_SPEED);
-                        }
                     }
                     break;
                 case Move_Off_White_Tape:
@@ -158,8 +152,9 @@ public class RedFarShootingAuto extends OpMode {
                     autoDone = true;
                     break;
             }
-
             // Send data to telemetry
+            speed = driveTrain.shooter.getCorrectedVelocity();
+            joinedTelemetry.addData("Shooter Speed", speed);
             joinedTelemetry.addData("Xcor",driveTrain.getXPosition());
             joinedTelemetry.addData("Ycor",driveTrain.getYPosition());
             joinedTelemetry.addData("X-cord", driveTrain.odometer.getPosX());
@@ -167,11 +162,10 @@ public class RedFarShootingAuto extends OpMode {
             joinedTelemetry.addData("Heading", pos.getHeading(AngleUnit.DEGREES));
             joinedTelemetry.addData("Current Waypoint",currentWayPoint.toString());
             joinedTelemetry.addData("Destination X",currentDestination.x);
-            joinedTelemetry.addData("Deqstination Y",currentDestination.y);
-           // joinedTelemetry.addData("shooter", driveTrain.shooter.getVelocity());
-            joinedTelemetry.addData("shooter2", driveTrain.shooter.getVelocity());
+            joinedTelemetry.addData("Destination Y",currentDestination.y);
+            joinedTelemetry.addData("Run Time",getRuntime());
+            joinedTelemetry.addData("Time Diff",getRuntime() - startTime);
             joinedTelemetry.update();
-
         }
     }
 
